8837a76d6d4967c680109affdd7a38e035611917
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2015-05-15 19:47:04 +0200

    Bugfix for test

--- mo-sms-service-ws-server-test.sh
+++ mo-sms-service-ws-server-test.sh
@@ -8,5 +8,5 @@ fi
 
 timestamp=`date +%Y-%m-%dT%H:%M:%S`
 
-soapcli -v '{DeliverShortMessage:{sms:{recipient:123,originator:507998000,content:"TEST",transactionId:"d4b550bf40207900",timestamp:"$timestamp"}}}' \
+soapcli -v '{DeliverShortMessage:{sms:{recipient:123,originator:507998000,content:"TEST",transactionId:"d4b550bf40207900",timestamp:"'$timestamp'"}}}' \
     mo-sms-service-ws.wsdl ${1:-http://localhost:5000/}
